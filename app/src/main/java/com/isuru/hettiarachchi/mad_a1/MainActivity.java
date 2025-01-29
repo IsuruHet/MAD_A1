e0a0e5349064143062928b2c2adf4b4fc5e68a4d
Isuru Hettiarachchi <113895873+IsuruHet@users.noreply.github.com>
2025-01-29 19:44:03 +0530

    first commit

---
+++ app/src/main/java/com/isuru/hettiarachchi/mad_a1/MainActivity.java
@@ -0,0 +1,17 @@
+package com.isuru.hettiarachchi.mad_a1;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
